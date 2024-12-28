bf5e6ee2b24a5faae7b4aa63c801e67755bd613e
hectorBack <hectorSalvador2001@hotmail.com>
2024-12-27 19:58:51 -0600

    creacion de la estructura del proyecto y las clases model

---
+++ src/main/java/com/curso/ecommerce/model/Orden.java
@@ -0,0 +1,57 @@
+package com.curso.ecommerce.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Orden {
+	
+	private Integer id;
+	private String numero;
+	private Date fechaCreacion;
+	private Date fechaRecibida;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	public Date getFechaRecibida() {
+		return fechaRecibida;
+	}
+	public void setFechaRecibida(Date fechaRecibida) {
+		this.fechaRecibida = fechaRecibida;
+	}
+	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.fechaCreacion = fechaCreacion;
+		this.fechaRecibida = fechaRecibida;
+	}
+	public Orden() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Orden [id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", fechaRecibida="
+				+ fechaRecibida + "]";
+	}
+	
+	
+	
+
+}
