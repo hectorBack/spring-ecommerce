bc1ebfc001b4f6964581d99e769bde931187570f
hectorBack <hectorSalvador2001@hotmail.com>
2025-01-01 11:25:56 -0600

    Redireccion al carrito de compras

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -40,5 +41,10 @@ public class HomeController {
         model.addAttribute("producto", producto);
 
         return "administrador/usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(){
+        return "administrador/usuario/carrito";
     }
 }
