c1b6dc538939364238ca4755ba7b0d74509105bb
hectorBack <hectorSalvador2001@hotmail.com>
2025-01-09 22:22:49 -0600

    cerrar sesion del usuario

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -81,4 +82,25 @@ public class UsuarioController {
         return "administrador/usuario/compras";
 
     }
+
+    @GetMapping("/detalle/{id}")
+    public String detalleCompra(@PathVariable Integer id, HttpSession session, Model model){
+        logger.info("Id de la orden: {}",id);
+        Optional<Orden> orden = ordenService.findById(id);
+
+        model.addAttribute("detalles", orden.get().getDetalleOrden());
+
+        //session
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+
+        return "administrador/usuario/detallecompra";
+    }
+
+    @GetMapping("/cerrar")
+    public String cerrarSesion( HttpSession session ) {
+        session.removeAttribute("idusuario");
+        return "redirect:/";
+    }
+
+
 }
--- src/main/java/com/curso/ecommerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/IOrdenService.java
@@ -4,11 +4,14 @@ import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrdenService {
 
     List<Orden> findAll();
+    Optional<Orden> findById(Integer id);
     Orden save (Orden orden);
     String generarNumeroOrden();
     List<Orden> findByUsuario(Usuario usuario);
+
 }
